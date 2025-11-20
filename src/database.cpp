abe3d729a08d5f9fbcb76cdcc8199b887d67d821
1yan4min24 <ebifebrianti89@gmail.com>
2025-11-20 11:07:05 -0300

    Update database.cpp

---
+++ src/database.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/database.hpp"
+using namespace std;
+vector<Ikan>dataTangkapan;
+vector<Ikan>lotLelang;
+vector <Ikan>stokTengkulak;
+vector<Ikan> pasar;
+vector<string> permintaanPasar; 
+vector <string> permintaanPasar = {
+    "Dolosi","Tuna", "Kombong", 
+};
